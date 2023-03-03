374fd6f06e9f94dd53f58a4a91d123aab236e995
lumanexx <techlumanex@gmail.com>
2023-03-03 16:12:53 +0000

    minor changes to app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,15 +26,18 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter,SimpleSpanProcess
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # HONEYCOMB----------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# x-ray----------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 
 #show this in the log within the backend-flask
 Simple_SpanProcessor = SimpleSpanProcessor(ConsoleSpanExporter())
