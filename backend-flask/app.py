ffe480e2e28214ad4c4b5e533c6618226b2bf751
lumanexx <techlumanex@gmail.com>
2023-03-04 09:24:38 +0000

    arranged app.y

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,7 +36,7 @@ provider.add_span_processor(processor)
 # x-ray----------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 
 #show this in the log within the backend-flask
